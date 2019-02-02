cfde169a32444f20c60ab64181024468b8430601
Prakhar Varshney <prakharnegative@gmail.com>
2019-02-03 00:53:47 +0530

    Update serializers.py

--- ctgapi/serializers.py
+++ ctgapi/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import getintouch
 
-class GetintouchSerializer(serializers.HyperlinkedModelSerializer):
+class GetintouchSerializer(serializers.ModelSerializer):
     class Meta:
         model = getintouch
-        fields = ('url','id','first_name','last_name','company','phone','email','website','comments')
+        fields = ('id','first_name','last_name','company','phone','email','website','comments')
